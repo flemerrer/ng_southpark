5f7f00cb8bea4fe1f15899afee85538902934834
francois <francois.lemerrer@pandata.ninja>
2026-01-22 12:18:53 +0100

    add service to inject characters into home

--- src/app/home/main-card/main-card.ts
+++ src/app/home/main-card/main-card.ts
@@ -1,47 +1,48 @@
-import {Component, computed, Input, Signal, signal, WritableSignal} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Component, computed, inject, Input, Signal, signal, WritableSignal} from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {TitleCasePipe} from "@angular/common";
 import {CharacterCard} from '../character-card/character-card';
 import {combineLatestAll} from 'rxjs';
 import {Character} from '../../types/character';
+import {Characters} from '../../services/characters.service';
 
 @Component({
-  selector: 'app-main-card',
-  imports: [
-    FormsModule,
-    TitleCasePipe,
-    CharacterCard,
-  ],
-  templateUrl: './main-card.html',
-  styleUrl: './main-card.css',
+	selector: 'app-main-card',
+	imports: [
+		FormsModule,
+		TitleCasePipe,
+		CharacterCard,
+	],
+	templateUrl: './main-card.html',
+	styleUrl: './main-card.css',
 })
 export class MainCard {
-  protected readonly title: string = 'Welcome to South Park'
-  protected readonly characters: Character[] = [
-    {id: 1, firstName: "Eric", lastName: "Cartman", img: "/characters/eric.jpg"},
-    {id: 2, firstName: "Stan", lastName: "Marsh", img: "/characters/stan.jpg"},
-    {id: 3, firstName: "Kenny", lastName: "McCormick", img: "/characters/kenny.jpg"},
-    {id: 4, firstName: "Kyle", lastName: "Broflovsky", img: "/characters/kyle.jpg"},
-  ];
-  count: number = 0
-  isDead: WritableSignal<boolean> = signal(false)
-  kennyStatus = computed(() => this.isDead())
-  addOne = () => {
-    this.count++
-    if (!this.isDead()) {
-      this.characters[2].img = "/characters/ghost_kenny.png"
-      this.isDead.set(true)
-    }
-    const countNode = document.getElementById("count")
-    if (countNode && this.count > 0  && this.count < 5) {
-      countNode.classList.add("blue")
-    } else if (countNode && this.count > 5  && this.count < 10) {
-      countNode.classList.remove("blue")
-      countNode.classList.add("purple")
-    } else if (countNode && this.count > 10)  {
-      countNode.classList.remove("purple")
-      countNode.classList.add("red")
-    }
-  };
-  name?: String
+	// Old Method of dependency injection
+	// constructor(private readonly characterService: Characters) {
+	// 	this.characters = this.characterService.getCharacters();
+	// }
+	private characterService = inject(Characters)
+	protected readonly title: string = 'Welcome to South Park'
+	protected readonly characters: Character[] = this.characterService.getCharacters();
+	count: number = 0
+	isDead: WritableSignal<boolean> = signal(false)
+	kennyStatus = computed(() => this.isDead())
+	addOne = () => {
+		this.count++
+		if (!this.isDead()) {
+			this.characters[2].img = "/characters/ghost_kenny.png"
+			this.isDead.set(true)
+		}
+		const countNode = document.getElementById("count")
+		if (countNode && this.count > 0 && this.count < 5) {
+			countNode.classList.add("blue")
+		} else if (countNode && this.count > 5 && this.count < 10) {
+			countNode.classList.remove("blue")
+			countNode.classList.add("purple")
+		} else if (countNode && this.count > 10) {
+			countNode.classList.remove("purple")
+			countNode.classList.add("red")
+		}
+	};
+	name?: String
 }
---
+++ src/app/services/characters.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Character} from '../types/character';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Characters {
+  // Mock DB
+  protected readonly characters: Character[] = [
+    {id: 1, firstName: "Eric", lastName: "Cartman", img: "/characters/eric.jpg"},
+    {id: 2, firstName: "Stan", lastName: "Marsh", img: "/characters/stan.jpg"},
+    {id: 3, firstName: "Kenny", lastName: "McCormick", img: "/characters/kenny.jpg"},
+    {id: 4, firstName: "Kyle", lastName: "Broflovsky", img: "/characters/kyle.jpg"},
+  ];
+
+  getCharacters = () => {return this.characters}
+}
